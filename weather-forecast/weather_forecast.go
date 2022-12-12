43e82bca3e37f70e23d7ce854599496266431a06
Jesserc <raymondjesse713@gmail.com>
2022-12-12 14:25:43 +0100

    Exercism task done

--- weather-forecast/weather_forecast.go
+++ weather-forecast/weather_forecast.go
@@ -1,8 +1,16 @@
+/*
+Package weather: Because we're dealing with a code for the Goblinocus weather report, this is the package title of this go file.
+The package contains details about the weather condition in Goblinocus.
+*/
 package weather
 
+// CurrentCondition holds the value for the current weather condition in Goblinocu.
 var CurrentCondition string
+
+// CurrentLocation holds the value for the current location in Goblinocus.
 var CurrentLocation string
 
+// Forecast function that tells us the current weather condition for a location in Goblinocus.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
